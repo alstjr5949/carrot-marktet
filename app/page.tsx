9755f3e077c29a7a685f7070bf55b7441df70edc
alstjr5949 <alstjr5949@gmail.com>
2024-10-30 22:31:25 +0900

    study: tailwind plugins

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center sm:bg-red-100 md:bg-green-100 lg:bg-blue-100 xl:bg-yellow-100 2xl:bg-purple-100">
       <a href="https://www.google.com">Google</a>
       <div className="w-full max-w-screen-sm bg-white shadow-md rounded-3xl p-4 flex flex-col gap-3">
+        <input type="text" className="input" />
         <button className="btn">Submit</button>
       </div>
     </main>
