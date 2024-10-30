539971dea4b1c847f8e28451e0bf4af9c349842e
alstjr5949 <alstjr5949@gmail.com>
2024-10-30 20:16:00 +0900

    study: list odd, even modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center sm:bg-red-100 md:bg-green-100 lg:bg-blue-100 xl:bg-yellow-100 2xl:bg-purple-100">
-      <div className="w-full max-w-screen-sm bg-white shadow-md rounded-3xl p-4 flex flex-col gap-2 md:flex-row ring ring-transparent transition-shadow *:outline-none has-[:invalid]:ring-red-500">
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 p-5 ring ring-transparent focus:ring-blue-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          placeholder="Search here..."
-          required
-        />
-        <span className="text-red-500 text-sm hidden peer-invalid:block">
-          Search is required.
-        </span>
-        <button className="bg-blue-500 text-white p-2 rounded-full active:scale-95 transition-transform font-medium md:px-4 peer-invalid:bg-red-500">
-          Search
-        </button>
+      <div className="w-full max-w-screen-sm bg-white shadow-md rounded-3xl p-4 flex flex-col gap-2">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-4 p-2.5 rounded-xl odd:bg-red-100 even:bg-blue-100"
+          >
+            <div className="size-10 rounded-full bg-gray-200" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="flex items-center justify-center size-6 rounded-full bg-yellow-200">
+              <span className="text-xs font-medium">{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
