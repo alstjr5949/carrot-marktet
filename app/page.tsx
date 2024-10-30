604702a79b7b4c409d23ff7c1242a8fd2b483a7f
alstjr5949 <alstjr5949@gmail.com>
2024-10-30 19:40:14 +0900

    study: form modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center sm:bg-red-100 md:bg-green-100 lg:bg-blue-100 xl:bg-yellow-100 2xl:bg-purple-100">
       <div className="w-full max-w-screen-sm bg-white shadow-md rounded-3xl p-4 flex flex-col gap-2 md:flex-row">
         <input
-          className="w-full rounded-full h-10 bg-gray-200 p-5 outline-none ring ring-transparent focus:ring-blue-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="text"
-          placeholder="Search..."
+          className="w-full rounded-full h-10 bg-gray-200 p-5 outline-none ring ring-transparent focus:ring-blue-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="email"
+          placeholder="Email address"
+          required
         />
-        <button className="bg-blue-500 text-white p-2 rounded-full outline-none active:scale-95 transition-transform font-medium md:px-4">
+        <span className="text-red-500 text-sm hidden peer-invalid:block">
+          Email is required.
+        </span>
+        <button className="bg-blue-500 text-white p-2 rounded-full outline-none active:scale-95 transition-transform font-medium md:px-4 peer-invalid:bg-red-500">
           Search
         </button>
       </div>
