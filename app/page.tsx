bcaa2075e6aa75e917e71953621bbef06d14d144
alstjr5949 <alstjr5949@gmail.com>
2024-10-30 22:19:32 +0900

    study: tailwind directives

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center sm:bg-red-100 md:bg-green-100 lg:bg-blue-100 xl:bg-yellow-100 2xl:bg-purple-100">
+      <a href="https://www.google.com">Google</a>
       <div className="w-full max-w-screen-sm bg-white shadow-md rounded-3xl p-4 flex flex-col gap-3">
-        <button className="w-full h-10 bg-red-400 text-white px-4 py-2 rounded-full">
-          Submit
-        </button>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
